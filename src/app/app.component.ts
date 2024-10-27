0af3ff806a00b977e396f66ad78ed2df405a59f2
rodrigo <lochi.perez@gmail.com>
2024-10-26 23:06:43 -0300

    inicio de proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/index/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    HeaderComponent,
+  FooterComponent,
+  MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/work-description/lets-talk/lets-talk.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lets-talk',
+  standalone: true,
+  imports: [],
+  templateUrl: './lets-talk.component.html',
+  styleUrl: './lets-talk.component.scss'
+})
+export class LetsTalkComponent {
+
+}
